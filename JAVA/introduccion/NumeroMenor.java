ffe128be27f6619c71184ec30d42236ff355e0be
Kevin Cabeza Robledo <kevin_xecster@hotmail.es>
2019-11-13 09:40:53 +0100

    carpeta java1

---
+++ JAVA/introduccion/NumeroMenor.java
@@ -0,0 +1,10 @@
+package estructurasdecontrol;
+
+public class NumeroMenor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JAVA/introduccion/practicar.java
@@ -0,0 +1,23 @@
+package estructurasdecontrol;
+
+import java.util.Scanner;
+
+public class practicar {
+
+	public static void main(String [] args) {
+		 
+	Scanner entrada = new Scanner(System.in) ;	
+	
+		
+	
+	//este ejercicio es para practiar
+	
+		float num1 = entrada.nextFloat();
+		float num2 = entrada.nextFloat();
+		int entero1 = entrada.nextInt();
+		int entero2  = entrada.nextInt();
+		
+		int suma
+		
+	}	
+}
